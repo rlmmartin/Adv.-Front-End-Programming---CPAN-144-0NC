561613f1d1139ad816474076c6047296a084e362
JessPacella <n01278536@humber.ca>
2026-04-05 16:33:23 -0400

    April5

---
+++ pages/catalog.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+
+/* somewhere here is where the ApI logic will be and where the Cards for the words will be */
+
+export default function Catalog() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <Head>
+        <title>WordsMall/Catalog</title>
+      </Head>
+
+      <main className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600">The Creative Catalog</h1>
+        <p className="mt-4 text-lg text-gray-700">
+          On this page, you can search our massive dictionary and find awesome, astounding and cool words!!!
+        </p>
+        <div className="mt-8 p-4">
+            {/* down here is where we will have a searchbar and where the words will appear */}
+          <p className="max-w-md mx-auto text-gray-600">
+            ------Nb. Our search bar 4 the site and the word cards will be constructed here pretty soon.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+/* So this is our homepage, yo jess, u gonna add the navigation links to catalog and cart pages here */
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <Head>
+        <title>WordsMall/Home</title>
+      </Head>
+
+      <main className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600">WordsMall Inc</h1>
+        <p className="mt-4 text-lg text-gray-700">
+          Welcome to WordsMall!
+        </p>
+        
+        <div className="mt-8 p-4">
+          <p className="max-w-md mx-auto text-gray-600">
+            We have all of your vocabulary needs and more! Feel free to look through 
+            our catalog of English Words! I am sure that it will be a 
+            great learning experience!
+          </p>
+        </div>
+        {/* For april 3rd, the navbar and footer components will go in the main tag, so somewhere around here i think*/}
+      </main>
+    </div>
+  );
+}
+
