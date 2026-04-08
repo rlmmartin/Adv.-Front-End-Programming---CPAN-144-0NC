325a2d80bced48838360b6a21f04f9bc6066706a
fushdd <daniil.vladimirov.07@gmail.com>
2026-04-08 16:07:13 -0400

    Added API logic

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,19 +1,58 @@
 import Head from 'next/head';
 import WordCard from '../components/Wordcard';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import axios from "axios";
 import cartContext from '../context/CartContext';
 
 /* somewhere here is where the ApI logic will be and where the Cards for the words will be */
 
 export default function Catalog() {
 
+  const [words, setWords] = useState([]);
+  const [loading, setloading] = useState(false);
   //This grabs the addtoCart function so we can add words from this page.
   const { addtoCart } = useContext(cartContext);
 
-  //This is just for testing this will be replaced with API - Danil
-  const testWords = [
-    { id:1, word: "Six-Seven", definition: "67 is a 2025 viral slang term, internet meme" }
-  ];
+  // fills the "words" array with 0-5 random words 
+  // (dictionary API may not have the definitions for some words)
+  async function getWords() {
+    // API call goes here
+    let newWords = [];
+    setloading(true);
+    for (let i = 0; i < 5; i++) {
+      try {
+        // get random word
+        const wordRes = await axios.get("https://api.api-ninjas.com/v2/randomword",
+            {
+              headers: {
+                "X-Api-Key": "W3hfiC0HdKZdHFDqzxk3BQYfmdK0ekQc96NoM0u6"
+              }
+            }
+          );
+        // get definition
+        const defRes = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordRes.data[0]}`);
+
+        // get random definition
+        const meanings = defRes.data[0].meanings;
+        const definitions = meanings[Math.floor(Math.random() * meanings.length)].definitions;
+        const randomDefinition = definitions[Math.floor(Math.random() * definitions.length)].definition;
+       
+        console.log(defRes)
+        newWords.push({
+          id: i + 1,
+          word: defRes.data[0].word,
+          definition: randomDefinition,
+        })
+      } catch {}
+    }
+    setloading(false);
+    setWords(newWords)
+  }
+
+  // //This is just for testing this will be replaced with API - Danil
+  // const testWords = [
+  //   { id:1, word: "Six-Seven", definition: "67 is a 2025 viral slang term, internet meme" }
+  // ];
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
@@ -29,7 +68,13 @@ export default function Catalog() {
         
         <div className="mt-8 p-4">
           {/* This will show a card for each word and when you click the buy button it will add it to the cart. */}
-          {testWords.map((item) => (
+          {/* "Loading..." while loading words */}
+          {/* "Nothing Found" if no words were found */}
+          {loading 
+          ? <div>Loading...</div>
+          : words.length === 0
+          ? <div>No words found. Click "Roll" to generate words</div>
+          : words.map((item) => (
             <WordCard
               key={item.id}
               word={item.word}
@@ -37,7 +82,10 @@ export default function Catalog() {
               onBuy={() => addtoCart(item)} 
             />
           ))}
-
+          <button 
+            onClick={() => getWords()}
+            className="mt-4 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-700 font-semibold"
+          >Roll</button>
             {/* down here is where we will have a searchbar and where the words will appear */}
           <p className="max-w-md mx-auto text-gray-600">
             ------Nb. Our search bar 4 the site and the word cards will be constructed here pretty soon.
